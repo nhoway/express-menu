ced4779f1f2b61bbdab82c1cf500be72e33db464
nicolas.hourmant <n.hourmant@syntesia.fr>
2017-05-20 15:51:34 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'menu_id':            'id',
+  'menu_label':         'name',
+  'menu_parent':        'parent',
+  'menu_icon':          'icon',
+  'menu_key':           'slug',
+  'menu_order':         'order',
+
+  'nav_tag_open':       '<ul>',
+  'nav_tag_close':      '</ul>',
+
+  'item_tag_open':      '<li>',
+  'item_tag_close':     '</li>',
+  'item_active_class':  'active',
+  'item_divider':       '<li class="divider"></li>',
+
+  'parentl1_tag_open':  '<li>',
+  'parentl1_tag_close': '</li>',
+
+  'parent_tag_open':    '<li>',
+  'parent_tag_close':   '</li>',
+  'parent_anchor_tag':  '<a href="%s">%s</a>',
+
+  'children_tag_open':  '<ul>',
+  'children_tag_close': '</ul>'
+};
---
+++ index.js
@@ -0,0 +1,93 @@
+/*jslint sloppy:true indent:2 plusplus:true regexp:true*/
+
+/**
+ * Module exports.
+ * @public
+ */
+
+module.exports = Menu;
+
+/**
+ * Menu generation class.
+ *
+ * @param {object} [options]
+ * @param {object} [options.items] Items to include in the menu
+ * @param {object} [options.configuration] Configuration set for the menu
+ * @public
+ */
+
+function Menu (options) {
+  if (!(this instanceof Menu)) {
+    return new Menu(options);
+  }
+
+  var opts = options || {}
+
+  var items = opts.items !== undefined
+    ? opts.items
+    : [];
+
+  if (typeof items !== 'object') {
+    throw new TypeError('option items must be an object');
+  }
+
+  var configuration = opts.configuration !== undefined
+    ? opts.configuration
+    : [];
+
+  if (typeof configuration !== 'object') {
+    throw new TypeError('option configuration must be an object');
+  }
+
+  this.items = items
+  //this.conf = require('menu-config');
+  this.conf = configuration
+}
+
+/**
+ * Render the menu as HTML code.
+ * @public
+ */
+
+Menu.prototype.render = function render () {
+  var html = this.conf['nav_tag_open'];
+  this.items = this._prepareItems(this.items);
+  var itemsKeys = Object.keys(this.items);
+  console.log(itemsKeys);
+  for (var i = 0; i < itemsKeys.length; i++) {
+    html += this._renderItem(this.items[itemsKeys[i]]);
+  }
+  html += this.conf['nav_tag_close'];
+  console.log(">>> END <<<")
+  return html;
+}
+
+/**
+ * Prepare menu items setting each item its children
+ * @private
+ */
+Menu.prototype._prepareItems = function prepareItems(data, parent = null) {
+  var items = {};
+  for (var i = 0; i < data.length; i++) {
+    if (data[i][this.conf['menu_parent']] == parent) {
+      items[data[i][this.conf['menu_order']]] = data[i];
+      items[data[i][this.conf['menu_order']]]['children'] = this._prepareItems(
+        data,
+        data[i][this.conf['menu_id']]
+      );
+    }
+  }
+  return items;
+}
+
+/**
+ * Render menu item with its children
+ * @private
+ */
+Menu.prototype._renderItem = function renderItem(data, prof = 0) {
+  return this.conf['item_tag_open'] +
+  '<a href="' + data['slug'] + '">' +
+    '[icon = ' + data['icon'] + '] ' + data['name'] +
+  '</a>' +
+  this.conf['item_tag_close'];
+}
