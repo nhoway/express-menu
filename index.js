950c5a5e119d57aecb6fdb4267d00f78816f156e
nhoway <n.hourmant@ovh.fr>
2017-05-22 22:36:29 +0200

    Adding full item rendering and depth management

--- index.js
+++ index.js
@@ -55,24 +55,41 @@ function Menu (options) {
 Menu.prototype.render = function render () {
   var html = this.conf['nav_tag_open'];
   this.items = this._prepareItems(this.items);
-  var itemsKeys = Object.keys(this.items);
-  console.log(itemsKeys);
-  for (var i = 0; i < itemsKeys.length; i++) {
-    html += this._renderItem(this.items[itemsKeys[i]]);
+  for (var key in this.items) {
+    html += this._renderItem(this.items[key]);
   }
   html += this.conf['nav_tag_close'];
-  console.log(">>> END <<<")
   return html;
+}
+
+/**
+ * Add menu item.
+ * @public
+ *
+ * @param {object} [items] Items to add in the menu
+ */
+
+Menu.prototype.addItems = function addItems (items) {
+  var itemsToAdd = items !== undefined
+    ? items
+    : [];
+  for (var i = 0; i < itemsToAdd.length; i++) {
+    this.items.append(itemsToAdd[i]);
+  }
+  return this;
 }
 
 /**
  * Prepare menu items setting each item its children
  * @private
+ *
+ * @param {object} [data] Items to add to parent
+ * @param {integer} [parent] Id of the parent
  */
 Menu.prototype._prepareItems = function prepareItems(data, parent = null) {
   var items = {};
   for (var i = 0; i < data.length; i++) {
-    if (data[i][this.conf['menu_parent']] == parent) {
+    if (data[i][this.conf['menu_parent']] == parent && data[i][this.conf['menu_id']] != parent) {
       items[data[i][this.conf['menu_order']]] = data[i];
       items[data[i][this.conf['menu_order']]]['children'] = this._prepareItems(
         data,
@@ -86,11 +103,78 @@ Menu.prototype._prepareItems = function prepareItems(data, parent = null) {
 /**
  * Render menu item with its children
  * @private
+ *
+ * @param {object} [data] Items to render
+ * @param {integer} [depth] Depth in the parent/child three
  */
-Menu.prototype._renderItem = function renderItem(data, prof = 0) {
-  return this.conf['item_tag_open'] +
-  '<a href="' + data['slug'] + '">' +
-    '[icon = ' + data['icon'] + '] ' + data['name'] +
-  '</a>' +
-  this.conf['item_tag_close'];
+Menu.prototype._renderItem = function renderItem(data, depth = 0) {
+  var html = this.conf['item_tag_open'];
+  html += '<a href="' + data[this.conf['menu_link']] + '">';
+
+  if (data[this.conf['menu_icon']] !== undefined) {
+    html += String.format(
+      this.conf['item_html_icon'],
+      data[this.conf['menu_icon']]
+    );
+  }
+  else {
+    html += String.format(
+      this.conf['item_html_icon'],
+      this.conf['default_menu_icon']
+    );
+  }
+
+  if (data[this.conf['menu_label']] !== undefined) {
+    html += String.format(
+      this.conf['item_html_label'],
+      data[this.conf['menu_label']]
+    );
+  }
+
+  if (typeof data[this.conf['menu_pins']] === 'object' || Object.keys(data['children']).length > 0) {
+    html += this.conf['item_pin_open'];
+    if (Object.keys(data['children']).length > 0) {
+      html += this.conf['item_pin_child'];
+    }
+    if (typeof data[this.conf['menu_pins']] === 'object') {
+      for (var i = data[this.conf['menu_pins']].length - 1; i >= 0; i--) {
+        html += String.format(
+          this.conf['item_pin_html'],
+          data[this.conf['menu_pins']][i]['content'],
+          data[this.conf['menu_pins']][i]['class']
+        );
+      }
+    }
+    html += this.conf['item_pin_close'];
+  }
+
+  html += '</a>';
+
+  if (Object.keys(data['children']).length > 0) {
+    html += this.conf['children_tag_open'];
+    for (var child in data['children']) {
+      html += this._renderItem(data['children'][child], depth + 1);
+    }
+    html += this.conf['children_tag_close'];
+  }
+
+  html += this.conf['item_tag_close'];
+  return html;
+}
+
+/**
+ * Adding String Format Method
+ *
+ */
+
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
 }
--- menu-config.js
+++ menu-config.js
@@ -1,25 +1,31 @@
 module.exports = {
   'menu_id':            'id',
-  'menu_label':         'name',
+  'menu_order':         'order',
+  'menu_label':         'label',
+  'menu_link':          'link',
   'menu_parent':        'parent',
   'menu_icon':          'icon',
-  'menu_key':           'slug',
-  'menu_order':         'order',
+  'menu_pins':          'pins',
+
+  'default_menu_icon':  'fa fa-circle-o',
 
   'nav_tag_open':       '<ul>',
   'nav_tag_close':      '</ul>',
 
   'item_tag_open':      '<li>',
-  'item_tag_close':     '</li>',
   'item_active_class':  'active',
+
+  'item_html_icon':     '<i class="fa {0}"></i>',
+  'item_html_label':    '<span>{0}</span>',
+
+  'item_pin_open':      '<span class="pull-right-container">',
+  'item_pin_child':     '<i class="fa fa-angle-left pull-right"></i>',
+  'item_pin_html':      '<small class="label pull-right {1}">{0}</small>',
+  'item_pin_close':     '</span>',
+
+  'item_tag_close':     '</li>',
+
   'item_divider':       '<li class="divider"></li>',
-
-  'parentl1_tag_open':  '<li>',
-  'parentl1_tag_close': '</li>',
-
-  'parent_tag_open':    '<li>',
-  'parent_tag_close':   '</li>',
-  'parent_anchor_tag':  '<a href="%s">%s</a>',
 
   'children_tag_open':  '<ul>',
   'children_tag_close': '</ul>'
